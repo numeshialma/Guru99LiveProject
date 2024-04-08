cb1821045e97b115753186d9a4c5acb5658bbf41
Numeshi <numeshialma@gmail.com>
2024-04-08 17:50:19 +0530

    Added new codes

--- Guru99LiveProject/src/test/java/testScripts/TestScript05.java
+++ Guru99LiveProject/src/test/java/testScripts/TestScript05.java
@@ -3,30 +3,26 @@ package testScripts;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
+import java.io.File;
 
 import static testScripts.Util.*;
 
 /**
 
  @author Numeshi Alma
- Test Script 04
+ Test Script 05
 
- Test Steps
- 1)  Go to http://www.demo.guru99.com/V4/
- 2) Enter valid UserId
- 3) Enter valid Password
- 4) Click Login
- 5) Verify ManagerID + Welcome To Manager's Page of Guru99 Bank
+ Take Screenshot
+
+
  */
 
-public class TestScript04 {
+public class TestScript05 {
 
     public static void main(String[] args) {
 
@@ -37,6 +33,14 @@ public class TestScript04 {
     WebDriver driver = new ChromeDriver();
     driver.get(BASE_URL);
 
+    try{
+        File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        screenshot.renameTo(new File("screenshot.png"));
+    } catch (Exception e){
+        e.getMessage();
+    }
+
+
     driver.findElement(By.name("uid")).clear();
     driver.findElement(By.name("uid")).sendKeys(USER_NAME);
 
@@ -45,7 +49,7 @@ public class TestScript04 {
 
     driver.findElement(By.name("btnLogin")).click();
 
-
+/*
     // Verify ManagerID + Welcome To Manager's Page of Guru99 Bank
     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(100));
     WebElement welcomeMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Welcome To Manager's Page of Guru99 Bank")));
@@ -67,6 +71,15 @@ public class TestScript04 {
         } else {
         System.out.println("Test case : Failed - Failed to find manager ID element ");
     }
+
+
+ */
+        try{
+            File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            screenshot.renameTo(new File("screenshot1.png"));
+        } catch (Exception e){
+            e.getMessage();
+        }
 
     driver.close();
 
